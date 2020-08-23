04e28c438b643647335c3e690b42f7920bca0f9a
Ahsan Sabri <ahsan.sabri_me08@yahoo.com>
2020-08-23 07:54:43 +0600

    first commit

---
+++ resources/js/routes.js
@@ -0,0 +1,28 @@
+let login = require('./components/auth/login.vue').default;
+let register = require('./components/auth/register.vue').default;
+let forget = require('./components/auth/forget.vue').default;
+let logout = require('./components/auth/logout.vue').default;
+
+// home
+let home = require('./components/home.vue').default;
+
+// Empoyee Component 
+let storeemployee = require('./components/employee/create.vue').default;
+let employee = require('./components/employee/index.vue').default;
+let editemployee = require('./components/employee/edit.vue').default;
+
+
+export const routes = [
+    { path: '/', component: login, name: '/' },
+    { path: '/register', component: register, name: 'register' },
+    { path: '/forget', component: forget, name: 'forget' },
+    { path: '/logout', component: logout, name: 'logout' },
+
+    // home
+    { path: '/home', component: home, name: 'home' },
+
+    // Employee Routes
+    { path: '/store-employee', component: storeemployee, name: 'store-employee' },
+    { path: '/employee', component: employee, name: 'employee' },
+    { path: '/edit-employee/:id', component: editemployee, name: 'edit-employee' },
+]
